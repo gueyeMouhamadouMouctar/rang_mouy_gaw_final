fa65b830a0bb85d92a662ff1f082f63c75d78ce9
maimouna96 <77724885+maimouna96@users.noreply.github.com>
2022-04-03 13:19:23 +0200

    superviseur
    superviseur

--- fileDattente/gestionFile/super_views.py
+++ fileDattente/gestionFile/super_views.py
@@ -7,15 +7,17 @@ from fileDattente import fileDattente
 from .models import ClientQueueManager
 
 
-def show(request):
-    return HttpResponse('salut show')
+def listeFA(request):
+    liste = ClientQueueManager().get_all_clients()
+    context = {'liste': liste}
+    return render(request, 'espace_superviseur.html', context)
 
 
 def start(request):
     return HttpResponse('salut start')
 
 
-def listeFA(request):
-    fileDattente = ClientQueueManager().get_all_clients()
-    print(fileDattente)
-    return render(request, 'espace_superviseur.html',  {'listeFileDattente': fileDattente})
+# def listeFA(request):
+#     fileDattente = ClientQueueManager().get_all_clients()
+#     print(fileDattente)
+#     return render(request, 'espace_superviseur.html',  {'listeFileDattente': fileDattente})
